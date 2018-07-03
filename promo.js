c0a80d4976b2a461a46f9c3f77293606e57a1a0a
laurens.versluis <laurens.versluis@arup.com>
2018-07-03 17:48:08 +0200

    added navigation

--- promo.js
+++ promo.js
@@ -4,17 +4,60 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/laurensversluis/cjiw2nfgt97zz2sq8ennhpm7h',
     center: [5.001128, 52.008653],
-    zoom: 9.2
+    zoom: 10,
+    pitch: 60
 });
+// Adding navigation
+var nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-left');
+
+// Adding logo
+// var logo = doc
+
+
+// Adding layers and layer toggle
+var layers = [
+    {'ptal': 'ptal_500m_grid'},
+    {'ptal_extruded': 'ptal_500m_grid_extruded'},
+    {'stops': 'ptal_stops'},
+    {'network': 'ptal_network2'}
+    ];
 
 map.on('load', function() {
-  map.addLayer({
-    id: 'laurensversluis.326iwqo1',
-    type: 'fill',
-    source: {
-      type: 'vector',
-      url: 'mapbox://styles/laurensversluis/cji1q65d504h52snbn5b9shqq'
-    },
-    'source-layer': 'laurensversluis.326iwqo1'
-  });
+
+    for (var i = 0; i < layers.length; i++) {
+        console.log(layers[i]);
+        var layer_dict = layers[i];
+        var layer_ref = Object.keys(layer_dict)[0];
+        var layer_source = layer_dict[layer_ref];
+
+        var link = document.createElement('a') ;
+        link.href = '#';
+        link.className = 'active';
+        link.textContent = layer_ref;
+        link.source = layer_source;
+
+        link.onclick = function (e) {
+            var layer_source = this.source;
+            e.preventDefault();
+            e.stopPropagation();
+
+            var visibility = map.getLayoutProperty(layer_source, 'visibility');
+            if (visibility === 'visible') {
+                map.setLayoutProperty(layer_source, 'visibility', 'none');
+                this.className = '';
+            } else {
+                this.className = 'active';
+                map.setLayoutProperty(layer_source, 'visibility', 'visible');
+            }
+        };
+
+    var menu = document.getElementById('menu');
+    menu.appendChild(link);
+    }
+
+    //Adding logo
+
+
 });
+
