f801f6b98f6e6ab83aa2b66deb3da52d0982e5f2
Laurens Versluis <laurens.versluis@arup.com>
2018-07-05 21:22:09 +0200

    added sidebar

--- promo.js
+++ promo.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibGF1cmVuc3ZlcnNsdWlzIiwiYSI6ImNqaTFwOWVuMTB4a
 
 var map = new mapboxgl.Map({
     container: 'map',
-//    style: 'mapbox://styles/mapbox/streets-v9',
-    style: 'mapbox://styles/laurensversluis/cjiw2nfgt97zz2sq8ennhpm7h',
+   style: 'mapbox://styles/mapbox/streets-v9',
+    // style: 'mapbox://styles/laurensversluis/cjiw2nfgt97zz2sq8ennhpm7h',
     center: [5.001128, 52.008653],
     zoom: 10,
     pitch: 60
@@ -13,30 +13,37 @@ var nav = new mapboxgl.NavigationControl();
 map.addControl(nav, 'top-left');
 
 
-
-
 // Adding layers and layer toggle
 var layers = [
-    {'ptal': 'ptal_500m_grid'},
-    {'ptal_extruded': 'ptal_500m_grid_extruded'},
-    {'stops': 'ptal_stops'},
-    {'network': 'ptal_network2'}
+    {'PTAL': 'ptal_500m_grid'},
+    {'PTAL extruded': 'ptal_500m_grid_extruded'},
+    {'PT stops': 'ptal_stops'},
+    {'PT network': 'ptal_network2'}
     ];
 
 map.on('load', function() {
 
     for (var i = 0; i < layers.length; i++) {
-        console.log(layers[i]);
+        // Getting layer information
         var layer_dict = layers[i];
         var layer_ref = Object.keys(layer_dict)[0];
         var layer_source = layer_dict[layer_ref];
 
+        // Make only the first layer visible
+        if (i === 0) {
+            map.setLayoutProperty(layer_source, 'visibility', 'visible');
+        } else {
+            map.setLayoutProperty(layer_source, 'visibility', 'none');
+        }
+
+        // Generate toggle menu
         var link = document.createElement('a') ;
         link.href = '#';
-        link.className = 'active';
+        // link.className = 'active';
         link.textContent = layer_ref;
         link.source = layer_source;
 
+        // Implement toggling
         link.onclick = function (e) {
             var layer_source = this.source;
             e.preventDefault();
