718507e493659f2ba42dfaae6ab27edaab4388f4
Laurens Versluis <laurens.versluis@arup.com>
2018-07-06 18:19:58 +0200

    added title and working legend

--- promo.js
+++ promo.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibGF1cmVuc3ZlcnNsdWlzIiwiYSI6ImNqaTFwOWVuMTB4a
 
 var map = new mapboxgl.Map({
     container: 'map',
-   style: 'mapbox://styles/mapbox/streets-v9',
-    // style: 'mapbox://styles/laurensversluis/cjiw2nfgt97zz2sq8ennhpm7h',
+   // style: 'mapbox://styles/mapbox/streets-v9',
+    style: 'mapbox://styles/laurensversluis/cjiw2nfgt97zz2sq8ennhpm7h',
     center: [5.001128, 52.008653],
     zoom: 10,
     pitch: 60
@@ -21,20 +21,31 @@ var layers = [
     {'PT network': 'ptal_network2'}
     ];
 
+var legend = document.getElementById('legend');
+
+var legend_elements = {
+    'PTAL':{
+        ranges: [
+            '1a very poor',
+            '1b very poor',
+            '2 poor',
+            '3 moderate',
+            '4 good',
+            '5 very good',
+            '6a excellent',
+            '6b most excellent'],
+        colors: ['#1e038c', '#1d59d3', '#00dada', '#96ec62', '#fbeb00', '#fdca30', '#ff3300', '#bd0900']
+    }
+};
+
 map.on('load', function() {
 
+    // Adding layers to map and toggle menu
     for (var i = 0; i < layers.length; i++) {
         // Getting layer information
         var layer_dict = layers[i];
         var layer_ref = Object.keys(layer_dict)[0];
         var layer_source = layer_dict[layer_ref];
-
-        // Make only the first layer visible
-        if (i === 0) {
-            map.setLayoutProperty(layer_source, 'visibility', 'visible');
-        } else {
-            map.setLayoutProperty(layer_source, 'visibility', 'none');
-        }
 
         // Generate toggle menu
         var link = document.createElement('a') ;
@@ -58,9 +69,36 @@ map.on('load', function() {
                 map.setLayoutProperty(layer_source, 'visibility', 'visible');
             }
         };
-
     var menu = document.getElementById('menu');
     menu.appendChild(link);
+    }
+
+    // Adding legend
+    for (i = 0; i < Object.keys(legend_elements).length; i++) {
+        var layer = Object.keys(legend_elements)[i];
+        var layer_info = legend_elements[layer];
+        var ranges = layer_info['ranges'];
+        var colors = layer_info['colors'];
+
+        for (i = 0; i < ranges.length; i++) {
+            var range = ranges[i];
+            var color = colors[i];
+            var item = document.createElement('div');
+
+            // Create symbol
+            var key = document.createElement('span');
+            key.className = 'legend-key';
+            key.style.backgroundColor = color;
+            item.appendChild(key);
+
+            // Create label
+            var value = document.createElement('span');
+            value.innerHTML = range;
+            item.appendChild(key);
+            item.appendChild(value);
+
+            legend.appendChild(item);
+        }
     }
 
     // Adding logo
